d448b47460e423749bb7c420d7d2d916117e5072
Chad Lowe <pfmsoft.dev@gmail.com>
2021-03-21 16:13:42 -0700

    der started history

--- src/eve_esi/cli/history_cli.py
+++ src/eve_esi/cli/history_cli.py
@@ -7,7 +7,25 @@ def history():
 
 
 @click.command()
-def get():
+@click.option("--regions", "-r", type=click.STRING)
+@click.option("--type_ids", "-t", type=click.STRING)
+@click.option("--json", "-j", type=click.File)
+@click.option(
+    "--destination",
+    "-d",
+    type=click.Choice(["app-dir", "stdout"], case_sensitive=False),
+    default="app-dir",
+    show_default=True,
+)
+@click.option(
+    "--output-format",
+    "-o",
+    type=click.Choice(["json", "csv"], case_sensitive=False),
+    default="json",
+    show_default=True,
+)
+def get(regions, type_ids, json):
+
     click.echo("getting market history")
 
 
