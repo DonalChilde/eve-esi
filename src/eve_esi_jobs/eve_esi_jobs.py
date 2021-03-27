04ba563e0103f4053eed464deeb13f9eedd0bf00
Chad Lowe <pfmsoft.dev@gmail.com>
2021-03-26 18:22:44 -0700

    do_work_order function, worker_count function

--- src/eve_esi_jobs/eve_esi_jobs.py
+++ src/eve_esi_jobs/eve_esi_jobs.py
@@ -1,10 +1,12 @@
 """Main module."""
 import asyncio
-from typing import Dict, List, Sequence
+from math import ceil
+from typing import Dict, List, Optional, Sequence
 
 from eve_esi_jobs.esi_provider import EsiProvider
 from eve_esi_jobs.job_to_action import make_action_from_job
-from eve_esi_jobs.models import EsiJob, deserialize_json_job
+from eve_esi_jobs.model_helpers import pre_process_work_order
+from eve_esi_jobs.models import EsiJob, EsiWorkOrder
 from eve_esi_jobs.pfmsoft.util.async_actions.aiohttp import (
     AiohttpQueueWorker,
     do_aiohttp_action_queue,
@@ -19,18 +21,24 @@ def do_jobs(esi_jobs: Sequence[EsiJob], esi_provider: EsiProvider, worker_count=
     actions = []
     for esi_job in esi_jobs:
         action = make_action_from_job(esi_job, esi_provider)
-        # action.context["esi_job"] = esi_job
         actions.append(action)
     asyncio.run(do_aiohttp_action_queue(actions, workers))
     return esi_jobs
 
 
-def do_jobs_json(
-    esi_jobs_json: List[Dict], esi_provider: EsiProvider, worker_count=1
-) -> Sequence[EsiJob]:
-    esi_jobs: List[EsiJob] = []
-    for esi_job_json in esi_jobs_json:
-        esi_job = deserialize_json_job(esi_job_json)
-        esi_jobs.append(esi_job)
-        do_jobs(esi_jobs, esi_provider, worker_count)
-    return esi_jobs
+def do_work_order(
+    ewo: EsiWorkOrder, esi_provider: EsiProvider, worker_count: Optional[int] = None
+):
+    pre_process_work_order(ewo)
+    worker_count = get_worker_count(len(ewo.jobs), worker_count)
+    do_jobs(ewo.jobs, esi_provider, worker_count)
+
+
+def get_worker_count(job_count, worker_count: Optional[int]) -> int:
+    max_workers = 100
+    worker_calc = ceil(job_count / 10)
+    if worker_calc > max_workers:
+        worker_calc = max_workers
+    if worker_count is not None:
+        worker_calc = worker_count
+    return worker_calc
