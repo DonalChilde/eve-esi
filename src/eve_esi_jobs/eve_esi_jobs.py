c0d29aa7d37e11e0a795a31a39013550cfc21bd8
Chad Lowe <pfmsoft.dev@gmail.com>
2021-03-31 13:31:03 -0700

    added some control over worker count

--- src/eve_esi_jobs/eve_esi_jobs.py
+++ src/eve_esi_jobs/eve_esi_jobs.py
@@ -16,34 +16,42 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-def do_jobs(esi_jobs: Sequence[EsiJob], esi_provider: EsiProvider, worker_count=1):
-    """ May mutate esi_jobs """
-    workers = []
+def do_jobs(
+    esi_jobs: Sequence[EsiJob],
+    esi_provider: EsiProvider,
+    worker_count: Optional[int] = None,
+    max_workers: int = 100,
+):
+    worker_count = get_worker_count(len(esi_jobs), worker_count, max_workers)
+    factories = []
     for _ in range(worker_count):
-        workers.append(AiohttpQueueWorkerFactory())
+        factories.append(AiohttpQueueWorkerFactory())
     actions = []
     for esi_job in esi_jobs:
         action = make_action_from_job(esi_job, esi_provider)
         actions.append(action)
-    asyncio.run(queue_runner(actions, workers))
+    asyncio.run(queue_runner(actions, factories))
     return esi_jobs
 
 
 def do_work_order(
-    ewo: EsiWorkOrder, esi_provider: EsiProvider, worker_count: Optional[int] = None
+    ewo: EsiWorkOrder,
+    esi_provider: EsiProvider,
+    worker_count: Optional[int] = None,
+    max_workers: int = 100,
 ):
     pre_process_work_order(ewo)
-    worker_count = get_worker_count(len(ewo.jobs), worker_count)
+    worker_count = get_worker_count(len(ewo.jobs), worker_count, max_workers)
     do_jobs(ewo.jobs, esi_provider, worker_count)
 
 
-def get_worker_count(job_count, worker_count: Optional[int]) -> int:
-    max_workers = 100
+def get_worker_count(job_count, workers: Optional[int], max_workers: int = 100) -> int:
+    """Try to strike a reasonable balance between speed and DOS."""
     worker_calc = ceil(job_count / 10)
     if worker_calc > max_workers:
         worker_calc = max_workers
-    if worker_count is not None:
-        worker_calc = worker_count
+    if workers is not None:
+        worker_calc = workers
     return worker_calc
 
 
@@ -55,3 +63,13 @@ def deserialize_json_job(esi_job_json: Dict) -> EsiJob:
 def deserialize_json_work_order(esi_work_order_json: Dict) -> EsiWorkOrder:
     esi_work_order = EsiWorkOrder(**esi_work_order_json)
     return esi_work_order
+
+
+def serialize_job(esi_job: EsiJob) -> Dict:
+    serialized = esi_job.dict()
+    return serialized
+
+
+def serialize_work_order(ewo: EsiWorkOrder) -> Dict:
+    serialized = ewo.dict()
+    return serialized
