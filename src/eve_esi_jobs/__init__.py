0fb48d0171e0363653594b3ba385b87db75db69f
Chad Lowe <pfmsoft.dev@gmail.com>
2021-03-31 08:02:48 -0700

    bump for next version

--- src/eve_esi_jobs/__init__.py
+++ src/eve_esi_jobs/__init__.py
@@ -3,7 +3,7 @@ import logging
 
 __author__ = """Chad Lowe"""
 __email__ = "pfmsoft@gmail.com"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
