863a9548a7d32b479aea6376f986696918a1e528
Chad Lowe <pfmsoft.dev@gmail.com>
2021-04-04 10:21:05 -0700

    added test data

--- tests/eve_esi_jobs/conftest.py
+++ tests/eve_esi_jobs/conftest.py
@@ -1,10 +1,11 @@
 """ fixtures """
 import json
 import logging
+from dataclasses import dataclass
 from importlib import resources
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
-from typing import Optional
+from typing import Any, Dict, Optional
 
 import pytest
 from rich import inspect
@@ -13,8 +14,12 @@ from eve_esi_jobs import logger as app_logger
 from eve_esi_jobs.esi_provider import EsiProvider
 
 APP_LOG_LEVEL = logging.INFO
-resource_path: str = "tests.eve_esi_jobs.resources.schema"
-resource_name: str = "esi_schema_1.7.15.json"
+
+
+@dataclass
+class FileResource:
+    file_path: Path
+    data: Any
 
 
 @pytest.fixture(scope="session", name="logger")
@@ -54,15 +59,9 @@ def file_handler(
 
 @pytest.fixture(scope="session")
 def esi_provider(esi_schema):
-    # logger = logger_(test_log_path)
-    # schema = load_schema("latest")
-    provider: EsiProvider = EsiProvider(esi_schema)
+    provider: EsiProvider = EsiProvider(esi_schema.data)
     assert provider.schema is not None
     assert provider.schema["basePath"] == "/latest"
-    # logger.info(
-    #     "created esi_provider with schema version: %s",
-    #     provider.schema["info"]["version"],
-    # )
     assert provider.schema_version == "1.7.15"
     return provider
 
@@ -80,13 +79,38 @@ def test_app_dir_(tmp_path_factory):
     return app_dir_path
 
 
-@pytest.fixture(scope="session", name="esi_schema")
-def esi_schema_(logger) -> dict:
+@pytest.fixture(scope="session", name="sample_data")
+def sample_data_(logger) -> Dict[str, FileResource]:
+    resource_path: str = "tests.eve_esi_jobs.resources.data"
+    resource_names = [
+        "3-market-history-params-extras.csv",
+        "3-market-history-params-extras.json",
+        "3-market-history-params.csv",
+        "3-market-history-params.json",
+        "3-type-ids.csv",
+        "3-type-ids.json",
+        "3-wrong-params.csv",
+        "3-wrong-params.json",
+        "json-dict.json",
+        "empty-list.json",
+    ]
+    sample_data = {}
+    for resource_name in resource_names:
+        file_resource = make_file_resource(
+            resource_path=resource_path, resource_name=resource_name, logger=logger
+        )
+        sample_data[file_resource.file_path.name] = file_resource
+    return sample_data
+
+
+def make_file_resource(resource_path, resource_name, logger) -> FileResource:
     try:
-        with resources.open_text(resource_path, resource_name) as schema_file:
-            schema = json.load(schema_file)
-            logger.info("Loaded resource file %s from %s", resource_name, resource_path)
-            return schema
+        with resources.path(resource_path, resource_name) as data_path:
+            data = data_path.read_text()
+            logger.debug(
+                "Loaded resource file %s from %s", resource_name, resource_path
+            )
+            return FileResource(file_path=data_path, data=data)
     except Exception as ex:
         logger.exception(
             "Unable to load resource file %s from %s Error msg %s",
@@ -97,11 +121,16 @@ def esi_schema_(logger) -> dict:
         raise ex
 
 
-@pytest.fixture(scope="session")
-def esi_schema_path():
-    with resources.path(resource_path, resource_name) as path:
-        schema_path = path
-    return schema_path
+@pytest.fixture(scope="session", name="esi_schema")
+def esi_schema_(logger) -> FileResource:
+    resource_path: str = "tests.eve_esi_jobs.resources.schema"
+    resource_name: str = "esi_schema_1.7.15.json"
+    file_resource = make_file_resource(
+        resource_path=resource_path, resource_name=resource_name, logger=logger
+    )
+    json_data = json.loads(file_resource.data)
+    file_resource.data = json_data
+    return file_resource
 
 
 @pytest.fixture(autouse=True)
