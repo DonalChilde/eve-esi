a1105941a5e527f291375531523b9ea56732aaee
Chad Lowe <pfmsoft.dev@gmail.com>
2021-04-06 08:52:11 -0700

    update samples to output jobs as well

--- src/eve_esi_jobs/typer_cli/jobs.py
+++ src/eve_esi_jobs/typer_cli/jobs.py
@@ -3,7 +3,7 @@
 import logging
 from pathlib import Path
 from time import perf_counter_ns
-from typing import Optional
+from typing import Callable, Optional, Sequence
 
 import typer
 
@@ -11,8 +11,10 @@ from eve_esi_jobs import sample_work_orders
 from eve_esi_jobs.eve_esi_jobs import (
     deserialize_work_order_from_dict,
     do_work_order,
+    serialize_job,
     serialize_work_order,
 )
+from eve_esi_jobs.models import EsiJob, EsiWorkOrder
 from eve_esi_jobs.typer_cli.cli_helpers import (
     load_esi_work_order_json,
     save_string,
@@ -45,7 +47,7 @@ not find mistakes that can only be checked on the server, eg. a non-existant typ
 """,
     ),
 ):
-    """Load work orders and run them.
+    """Load Work Orders and run them.
 
     Dry-run will perform all operations up to but not including making the actual
     http requests. This will detect some missed settings and parameters, but does
@@ -77,10 +79,11 @@ def samples(
     ctx: typer.Context,
     path_out: str = typer.Argument("./tmp", help="Path to output directory."),
 ):
-    """"""
+    """Generate sample Work Orders and Jobs."""
     output_path_string = validate_output_path(path_out)
     output_path = Path(output_path_string) / Path("samples")
-    sample_list = [
+    typer.echo(f"Samples will be saved to {output_path.resolve()}")
+    ewo_list = [
         sample_work_orders.response_to_job_json_file,
         sample_work_orders.result_to_job_json_file,
         sample_work_orders.result_to_json_file_and_response_to_json_file,
@@ -89,31 +92,37 @@ def samples(
         sample_work_orders.result_to_csv_file,
         sample_work_orders.result_with_pages_to_json_file,
     ]
-    typer.echo(f"Sample Esi Work Orders will be saved to {output_path.resolve()}")
-    for sample in sample_list:
-        ewo = sample()
-        file_path = output_path / Path(ewo.name).with_suffix(".json")
+    for sample in ewo_list:
+        ewo: EsiWorkOrder = sample()
+        file_path = (
+            output_path / Path("work-orders") / Path(ewo.name).with_suffix(".json")
+        )
         ewo_string = serialize_work_order(ewo)
         save_string(ewo_string, file_path, parents=True)
-        # save_json(ewo.dict(), file_path, parents=True)
+    jobs_list: Sequence[Callable] = []
+    for sample in jobs_list:
+        job: EsiJob = sample()
+        file_path = output_path / Path("jobs") / Path(job.name).with_suffix(".json")
+        job_string = serialize_job(job)
+        save_string(job_string, file_path, parents=True)
     start = ctx.obj.get("start_time", perf_counter_ns())
     end = perf_counter_ns()
     seconds = (end - start) / 1000000000
     typer.echo(f"Task completed in {seconds:0.2f} seconds")
 
 
-def combine(ctx: typer.Context, source_path: Path, out_path: Path):
-    """combine all the jobs in a dir into one workorder."""
-    pass
+# def combine(ctx: typer.Context, source_path: Path, out_path: Path):
+#     """combine all the jobs in a dir into one workorder."""
+#     pass
 
 
-def bulk_create(ctx: typer.Context, op_id, input_file: Path, file_type: str):
-    """Bulk create jobs from json or csv files.
+# def bulk_create(ctx: typer.Context, op_id, input_file: Path, file_type: str):
+#     """Bulk create jobs from json or csv files.
 
-    json must be in list of dicts of valid params for op_id
-    csv must read into a list of dicts that contain the required params
-    extra params are ok.
-    should be able to read previous csv output that contains required fields
-    identical inputs will be consolidated. (dict to json.dumps, set, and back)
-    """
-    pass
+#     json must be in list of dicts of valid params for op_id
+#     csv must read into a list of dicts that contain the required params
+#     extra params are ok.
+#     should be able to read previous csv output that contains required fields
+#     identical inputs will be consolidated. (dict to json.dumps, set, and back)
+#     """
+#     pass
