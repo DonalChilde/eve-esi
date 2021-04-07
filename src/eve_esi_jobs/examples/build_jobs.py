13c7f768f90316478ec83922be3924e8ebd0d96c
Chad Lowe <pfmsoft.dev@gmail.com>
2021-04-07 10:03:10 -0700

    examples for late jinja templates

---
+++ src/eve_esi_jobs/examples/build_jobs.py
@@ -0,0 +1,34 @@
+"""Examples of job building methods, for use in building jinja templates."""
+from eve_esi_jobs import models
+
+
+def get_industry_facilities():
+    job = models.EsiJob(
+        op_id="get_industry_facilities",
+    )
+    job.callbacks.success.append(
+        models.JobCallback(callback_id="response_content_to_json")
+    )
+    job.callbacks.success.append(
+        models.JobCallback(
+            callback_id="save_esi_job_to_json_file",
+            kwargs={"file_path": "data/industry-facilities.json"},
+        )
+    )
+    return job
+
+
+def get_industry_systems():
+    job = models.EsiJob(
+        op_id="get_industry_systems",
+    )
+    job.callbacks.success.append(
+        models.JobCallback(callback_id="response_content_to_json")
+    )
+    job.callbacks.success.append(
+        models.JobCallback(
+            callback_id="save_esi_job_to_json_file",
+            kwargs={"file_path": "data/industry-systems.json"},
+        )
+    )
+    return job
