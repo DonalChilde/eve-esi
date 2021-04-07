476bc97dc619dc668e8abf016183d7022faba1f5
Chad Lowe <pfmsoft.dev@gmail.com>
2021-04-07 10:04:51 -0700

    add examples of jobs without workorder

--- src/eve_esi_jobs/examples/jobs.py
+++ src/eve_esi_jobs/examples/jobs.py
@@ -2,9 +2,7 @@ from eve_esi_jobs import models
 
 
 def get_industry_facilities():
-    job = models.EsiJob(
-        op_id="get_industry_facilities",
-    )
+    job = models.EsiJob(op_id="get_industry_facilities", name="get_industry_facilities")
     job.callbacks.success.append(
         models.JobCallback(callback_id="response_content_to_json")
     )
@@ -18,9 +16,7 @@ def get_industry_facilities():
 
 
 def get_industry_systems():
-    job = models.EsiJob(
-        op_id="get_industry_systems",
-    )
+    job = models.EsiJob(op_id="get_industry_systems", name="get_industry_systems")
     job.callbacks.success.append(
         models.JobCallback(callback_id="response_content_to_json")
     )
@@ -28,6 +24,21 @@ def get_industry_systems():
         models.JobCallback(
             callback_id="save_esi_job_to_json_file",
             kwargs={"file_path": "data/industry-systems.json"},
+        )
+    )
+    return job
+
+
+def post_universe_names():
+    job = models.EsiJob(op_id="post_universe_names", name="post_universe_names")
+    job.parameters = {"ids": [95465499, 30000142]}
+    job.callbacks.success.append(
+        models.JobCallback(callback_id="response_content_to_json")
+    )
+    job.callbacks.success.append(
+        models.JobCallback(
+            callback_id="save_esi_job_to_json_file",
+            kwargs={"file_path": "data/post_universe_names-${esi_job_uid}.json"},
         )
     )
     return job
