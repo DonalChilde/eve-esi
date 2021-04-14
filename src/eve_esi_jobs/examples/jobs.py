8d9e3cb8c45b435b69f87f922b13d7e8667b9251
Chad Lowe <pfmsoft.dev@gmail.com>
2021-04-14 15:44:06 -0700

    useful default callbacks for jobs

--- src/eve_esi_jobs/examples/jobs.py
+++ src/eve_esi_jobs/examples/jobs.py
@@ -1,105 +1,82 @@
 from typing import Optional
 
 from eve_esi_jobs import models
+from eve_esi_jobs.callback_manifest import DefaultCallbackProvider
 
 
 def get_industry_facilities(
-    default_callbacks: Optional[models.CallbackCollection] = None,
+    callbacks: Optional[models.CallbackCollection] = None,
 ):
-    if default_callbacks is None:
-        default_callbacks = models.CallbackCollection()
-    else:
-        default_callbacks = default_callbacks.copy(deep=True)
+    if callbacks is None:
+        callbacks = DefaultCallbackProvider().default_callback_collection()
+        callbacks.success.append(
+            models.JobCallback(
+                callback_id="save_esi_job_to_json_file",
+                kwargs={"file_path": "data/industry-facilities-esi-job.json"},
+            )
+        )
+        callbacks.success.append(
+            models.JobCallback(
+                callback_id="save_json_result_to_file",
+                kwargs={"file_path": "data/industry-facilities.json"},
+            )
+        )
     job = models.EsiJob(
         op_id="get_industry_facilities",
         name="get_industry_facilities",
-        callbacks=default_callbacks,
+        callbacks=callbacks,
     )
-    # job.callbacks.success.append(
-    #     models.JobCallback(callback_id="response_content_to_json")
-    # )
-    # job.callbacks.success.append(models.JobCallback(callback_id="response_to_esi_job"))
-    job.callbacks.success.append(
-        models.JobCallback(
-            callback_id="save_esi_job_to_json_file",
-            kwargs={"file_path": "data/industry-facilities-esi-job.json"},
-        )
-    )
-    job.callbacks.success.append(
-        models.JobCallback(
-            callback_id="save_json_result_to_file",
-            kwargs={"file_path": "data/industry-facilities.json"},
-        )
-    )
-    # job.callbacks.fail.append(models.JobCallback(callback_id="response_to_esi_job"))
-    # job.callbacks.fail.append(models.JobCallback(callback_id="log_job_failure"))
     return job
 
 
 def get_industry_systems(
-    default_callbacks: Optional[models.CallbackCollection] = None,
+    callbacks: Optional[models.CallbackCollection] = None,
 ):
-    if default_callbacks is None:
-        default_callbacks = models.CallbackCollection()
-    else:
-        default_callbacks = default_callbacks.copy(deep=True)
+    if callbacks is None:
+        callbacks = DefaultCallbackProvider().default_callback_collection()
+        callbacks.success.append(
+            models.JobCallback(
+                callback_id="save_esi_job_to_json_file",
+                kwargs={"file_path": "data/industry-systems-esi-job.json"},
+            )
+        )
+        callbacks.success.append(
+            models.JobCallback(
+                callback_id="save_json_result_to_file",
+                kwargs={"file_path": "data/industry-systems.json"},
+            )
+        )
     job = models.EsiJob(
         op_id="get_industry_systems",
         name="get_industry_systems",
-        callbacks=default_callbacks,
+        callbacks=callbacks,
     )
-    # job.callbacks.success.append(
-    #     models.JobCallback(callback_id="response_content_to_json")
-    # )
-    # job.callbacks.success.append(models.JobCallback(callback_id="response_to_esi_job"))
-    job.callbacks.success.append(
-        models.JobCallback(
-            callback_id="save_esi_job_to_json_file",
-            kwargs={"file_path": "data/industry-systems-esi-job.json"},
-        )
-    )
-    job.callbacks.success.append(
-        models.JobCallback(
-            callback_id="save_json_result_to_file",
-            kwargs={"file_path": "data/industry-systems.json"},
-        )
-    )
-    # job.callbacks.fail.append(models.JobCallback(callback_id="response_to_esi_job"))
-    # job.callbacks.fail.append(models.JobCallback(callback_id="log_job_failure"))
     return job
 
 
 def post_universe_names(
-    default_callbacks: Optional[models.CallbackCollection] = None,
+    callbacks: Optional[models.CallbackCollection] = None,
 ):
-    if default_callbacks is None:
-        default_callbacks = models.CallbackCollection()
-    else:
-        default_callbacks = default_callbacks.copy(deep=True)
+    if callbacks is None:
+        callbacks = DefaultCallbackProvider().default_callback_collection()
+        callbacks.success.append(
+            models.JobCallback(
+                callback_id="save_esi_job_to_json_file",
+                kwargs={
+                    "file_path": "data/post_universe_names-${esi_job_uid}-esi-job.json"
+                },
+            )
+        )
+        callbacks.success.append(
+            models.JobCallback(
+                callback_id="save_json_result_to_file",
+                kwargs={"file_path": "data/post_universe_names-${esi_job_uid}.json"},
+            )
+        )
     job = models.EsiJob(
         op_id="post_universe_names",
         name="post_universe_names",
-        callbacks=default_callbacks,
+        callbacks=callbacks,
     )
     job.parameters = {"ids": [95465499, 30000142]}
-    # job.callbacks.success.append(
-    #     models.JobCallback(callback_id="response_content_to_json")
-    # )
-    # job.callbacks.success.append(models.JobCallback(callback_id="response_to_esi_job"))
-    job.callbacks.success.append(
-        models.JobCallback(
-            callback_id="save_esi_job_to_json_file",
-            kwargs={
-                "file_path": "data/post_universe_names-${esi_job_uid}-esi-job.json"
-            },
-        )
-    )
-    job.callbacks.success.append(
-        models.JobCallback(
-            callback_id="save_json_result_to_file",
-            kwargs={"file_path": "data/post_universe_names-${esi_job_uid}.json"},
-        )
-    )
-    # job.callbacks.fail.append(models.JobCallback(callback_id="response_to_esi_job"))
-    # job.callbacks.fail.append(models.JobCallback(callback_id="log_job_failure"))
     return job
