33d145f4e4bcef865363743e272a1cbc55063480
Chad Lowe <pfmsoft.dev@gmail.com>
2021-05-09 13:33:33 -0700

    version bump

--- src/eve_esi_jobs/__init__.py
+++ src/eve_esi_jobs/__init__.py
@@ -14,7 +14,7 @@ from eve_esi_jobs.runners import do_jobs, do_workorder
 
 __author__ = """Chad Lowe"""
 __email__ = "pfmsoft@gmail.com"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = [
     "do_jobs",
